a2ecb018f081c49a8e21a35c161b8f8e3a83ce8d
Hiroki Suezawa <suezawa@gmail.com>
2022-02-21 21:45:14 +0900

    Add results

--- urlscan/result.go
+++ urlscan/result.go
@@ -1,1 +1,41 @@
 package urlscan
+
+import (
+	"encoding/json"
+	"github.com/pkg/errors"
+	"io"
+	"net/http"
+)
+
+type Result struct {
+	Verdicts struct {
+		Overall struct {
+			Score       int      `json:"score"`
+			Categories  []string `json:"categories"`
+			Brands      []string `json:"brands"`
+			Tags        []string `json:"tags"`
+			Malicious   bool     `json:"malicious"`
+			HasVerdicts bool     `json:"hasVerdicts"`
+		} `json:"overall"`
+	} `json:"verdicts"`
+}
+
+func GetResult(uuid string) (*Result, error) {
+	resp, err := http.Get("https://urlscan.io/api/v1/result/" + uuid + "/")
+	if err != nil {
+		return nil, errors.Errorf("Failed to get result: %s", err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Errorf("Failed to get result: %s", err)
+	}
+
+	var result Result
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, errors.Errorf("Failed to unmarshal result: %s", err)
+	}
+
+	return &result, nil
+}
--- urlscan/result_test.go
+++ urlscan/result_test.go
@@ -1,1 +1,13 @@
 package urlscan
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestGetResult(t *testing.T) {
+	uuid := "c623953a-74ed-408b-b3ba-22cb6eb2aaaa"
+	_, err := GetResult(uuid)
+
+	require.NoError(t, err)
+}
